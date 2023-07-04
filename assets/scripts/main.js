cb299726df7df4ac2f736939f8a3f0df52452f17
Lintfree <34984989+Lintfree@users.noreply.github.com>
2023-07-04 13:16:25 -0400

    overhaul
    redesign, deleted old

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+
+// Get the current date
+var currentDate = new Date();
+
+// Format the date as desired
+var formattedDate = currentDate.toDateString();
+
+// Insert the formatted date into the HTML element with the id "currentDate"
+document.getElementById("currentDate").innerHTML = formattedDate;
